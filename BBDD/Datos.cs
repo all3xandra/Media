7feab546ba8099255192b0aa085895c48c77160f
EHU <a@a.com>
2022-04-04 10:15:21 +0200

    Agregar archivos de proyecto.

---
+++ BBDD/Datos.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBDD
+{
+    public class Datos
+    {
+        private SqlConnection connection;
+
+        public Datos()
+        {
+            connect();
+        }
+
+        /// <summary>
+        /// Este método realiza la conexion a la base de datos.
+        /// </summary>
+        /// <returns>Boolean</returns>
+        public void connect()
+        {
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "tcp:hads2207.database.windows.net,1433";
+                builder.UserID = "apelipian001@ikasle.ehu.eus@hads2207";
+                builder.Password = "hadsaj2122@";
+                builder.InitialCatalog = "HADS22-07";
+
+                connection = new SqlConnection(builder.ConnectionString);
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public List<int> getHoras(String asign)
+        {
+            List<int> horas = new List<int>();
+
+            String sql = "SELECT EstudianteTarea.hReales FROM EstudianteTarea INNER JOIN TareaGenerica ON EstudianteTarea.codTarea = TareaGenerica.codigo WHERE (EstudianteTarea.codTarea = TareaGenerica.codigo AND TareaGenerica.codAsig = @asign)";
+            SqlCommand sqlCmd = new SqlCommand(sql, connection);
+            sqlCmd.Parameters.AddWithValue("@asign", asign);
+
+            using (SqlDataReader reader = sqlCmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    horas.Add(Int32.Parse(string.Format("{0}", reader["hReales"])));
+                }
+            }
+
+            return horas;
+        }
+    }
+}
---
+++ Media/Cliente.asmx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Media
+{
+    /// <summary>
+    /// Descripción breve de Cliente
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Cliente : System.Web.Services.WebService
+    {
+        private BBDD.Datos bd = new BBDD.Datos();
+        public int mediaHoras(String asign)
+        {
+            List<int> horas = bd.getHoras(asign);
+
+            return horas.Sum()/horas.Count();
+        }
+    }
+}
